a44b2212e5a8b49cd1281f9117d1e14f4ab96831
João Paulo Guedes Carvalho <joaopaullogcarvalho@gmail.com>
2025-10-20 17:30:00 -0300

    refactor(core/dependecies): use Annotated for dependency injection and integrate role verification with UserRole

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -11,22 +11,22 @@ from app import model
 security = HTTPBearer()
 
 async def get_current_user(credentials: Annotated[HTTPAuthorizationCredentials, Depends(security)], db: Annotated[Session, Depends(get_db)]) -> model.User:
-    """Retorna o usuário atual a partir do token JWT"""
+    """Retorna o usuário atual a partir do token JWT gerado"""
 
     token = credentials.credentials
     payload = verify_token(token)
 
     if not payload or not payload.get("sub"):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid or expired token", headers={"WWW-Authenticate": "Bearer"})
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid or expired token!", headers={"WWW-Authenticate": "Bearer"})
 
     user_repo = UserRepository(db)
     user = user_repo.get_by_email(payload["sub"])
 
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found", headers={"WWW-Authenticate": "Bearer"})
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found!", headers={"WWW-Authenticate": "Bearer"})
     
     if not user.active:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Inactive user")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Inactive user!")
 
     return user
 
