c9f8a32d3817ca833f823e3b5c6a4f5750e5324d
João Paulo Guedes Carvalho <joaopaullogcarvalho@gmail.com>
2025-10-29 22:06:38 -0300

    refactor(schema): Adjust schemas to support changes in RNC models.

--- app/schema/__init__.py
+++ app/schema/__init__.py
@@ -1,12 +1,12 @@
-from .part_schema import PartBase, PartCreate, PartRead
 from .user_schema import UserBase, UserCreate, UserRead, UserUpdate, UserLogin
+from .rnc_schema import RNCCreate, RNCRead, RNCUpdate
+from .part_schema import PartBase, PartCreate, PartRead
 from .token_schema import Token, TokenData
-from .rnc_schema import RNCBase, RNCCreate, RNCRead
 
 # Opcional: define o que é exportado quando se usa "from app.schemas import *"
 __all__ = [
     "PartBase", "PartCreate", "PartRead",
     "UserBase", "UserCreate", "UserRead", "UserUpdate", "UserLogin",
     "Token", "TokenData",
-    "RNCBase", "RNCCreate", "RNCRead",
+    "RNCCreate", "RNCRead", "RNCUpdate"
 ]
--- app/schema/rnc_schema.py
+++ app/schema/rnc_schema.py
@@ -5,28 +5,45 @@ from typing import Optional
 # Importamos os schemas dos quais ele depende
 from .user_schema import UserRead
 from .part_schema import PartRead
+from app import model
 
-class RNCBase(BaseModel):
-    num_rnc: str
-    title: str
-    status: str
-    critical_level: str
-    date_of_occurrence: datetime
+class RNCCreate(BaseModel):
     part_id: int
-    machine_id: Optional[int] = None
+    status: model.RNCStatus = model.RNCStatus.ABERTO
+    condition: model.RNCCondition = model.RNCCondition.EM_ANALISE
+    date_of_occurrence: datetime
+    critical_level: str
+    title: str
+    observations: Optional[str] = None
+    open_by_id: int
 
-class RNCCreate(RNCBase):
-    pass
+class RNCUpdate(BaseModel):
+    title: Optional[str] = None
+    critical_level: Optional[str] = None
+    status: Optional[model.RNCStatus] = None
+    condition: Optional[model.RNCCondition] = None
+    observations: Optional[str] = None
+    current_responsible_id: Optional[int] = None
+    closed_by_id: Optional[int] = None
 
-class RNCRead(RNCBase):
+class RNCRead(BaseModel):
     id: int
+    num_rnc: int
+    title: str
+    status: model.RNCStatus
+    condition: model.RNCCondition
+    observations: Optional[str] = None
+    part_id: int
+    open_by_id: int
+    current_responsible_id: Optional[int] = None
+    closed_by_id: Optional[int] = None
     opening_date: datetime
     closing_date: Optional[datetime] = None
-    open_by_id: int
-    
+
     # Usamos os schemas de leitura para aninhar os dados na resposta da API
     part: PartRead
-    open_by_id: UserRead
+    open_by: UserRead
+    closed_by: Optional[UserRead] = None
     
     class Config:
         from_attributes = True
