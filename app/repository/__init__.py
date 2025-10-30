6fea906c60d0374aa78eb7dc6bfc88e59f1fd0cc
João Paulo Guedes Carvalho <joaopaullogcarvalho@gmail.com>
2025-10-29 22:07:46 -0300

    feat(repository): Implement a repository for managing non-conformity reports (NCRs).

---
+++ app/repository/__init__.py
@@ -0,0 +1,10 @@
+from .user_repository import UserRepository
+from .rnc_repository import RNCRepository
+from .auth_repository import AuthRepository
+
+
+__all__ = [
+    "UserRepository",
+    "RNCRepository",
+    "AuthRepository"
+]
