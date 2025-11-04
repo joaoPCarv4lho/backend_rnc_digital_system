81127fc672b20043e42b5a68e0271f318ccde463
João Paulo Guedes Carvalho <joaopaullogcarvalho@gmail.com>
2025-11-04 08:36:23 -0300

    feat(service): Implement logic for searching for RNCs and parts. -Adds get_rncs_by_user to return RNCs from a user. -Implements get_filtered_rncs with validation for the filters. -Adds PartService to encapsulate the parts search logic. -Updates __init__.py to export PartService.

--- app/service/__init__.py
+++ app/service/__init__.py
@@ -1,9 +1,11 @@
 from .auth_service import AuthService
 from .rnc_service import RNCService
 from .user_service import UserService
+from .part_service import PartService
 
 __all__ = [
     "AuthService",
     "RNCService",
-    "UserService"
+    "UserService",
+    "PartService"
 ]
